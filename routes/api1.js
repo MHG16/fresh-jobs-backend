5102289668f704e387b31f063033d6510ccc400e
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-19 15:38:32 -0500

    added routes to routes/api1.js

--- routes/api1.js
+++ routes/api1.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let router = express.Router();
+let bookshelfApi = require('bookshelf-api');
+let path = require('path');
+let configuredApi = bookshelfApi({
+	path: path.join(__dirname, '..', 'models')
+});
 
-// router.get('/products', function(req, res, next) {
+router.use('/', configuredApi);
 
-// });
 
 module.exports = router;
